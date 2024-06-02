4963fbcd398946b1eef15fbe57c4ae542042588f
Sercan Ümit ÖNER <sercanumitoner@gmail.com>
2024-06-02 19:51:57 +0300

    Added user profile page and editing functionalities.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 import sqlalchemy as sa
 from app import db
 from app.models import User
+from wtforms import TextAreaField
+from wtforms.validators import Length
+from flask_wtf.file import FileAllowed
+
 
 class LoginForm(FlaskForm):
     username = StringField('Kullanıcı Adı', validators=[DataRequired()])
@@ -29,3 +33,27 @@ class RegistrationForm(FlaskForm):
             User.email == email.data))
         if user is not None:
             raise ValidationError('Bu email adresi zaten alınmış.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField(
+        'Kullanıcı Adı',
+        validators=[
+            Length(min=2, message='Kullanıcı adı kısmı 2 karakterden kısa olamaz.'),
+            Length(max=25, message='Kullanıcı adı kısmı 25 karakterden uzun olamaz.'),
+            DataRequired(message='Kullanıcı adı zorunludur.')
+        ]
+    )
+    about_me = TextAreaField(
+        'Hakkında',
+        validators=[
+            Length(min=0, max=500, message='Hakkında kısmı 500 karakterden uzun olamaz.')
+        ]
+    )
+    profile_picture = FileField(
+        'Profil Resmi Güncelle',
+        validators=[
+            FileAllowed(['jpg', 'png'], message='Profil resmi yalnızca jpg veya png formatında olmalıdır.')
+        ]
+    )
+    submit = SubmitField('Güncelle')
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,22 @@
 from urllib.parse import urlsplit
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, get_flashed_messages
 from flask_login import login_user, logout_user, current_user, login_required
 import sqlalchemy as sa
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
+from datetime import datetime, timezone
+from app.forms import EditProfileForm
+from werkzeug.utils import secure_filename
+import os
 
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_login_date = datetime.now(timezone.utc)
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index')
@@ -15,7 +26,7 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('homepage'))
     form = LoginForm()
     if form.validate_on_submit():
         user = db.session.scalar(
@@ -45,7 +56,7 @@ def logout():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('homepage'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -56,3 +67,45 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = db.first_or_404(sa.select(User).where(User.username == username))
+    posts = [
+        {'author': user, 'post_text': 'Test post #1'},
+        {'author': user, 'post_text': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
+@app.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        
+        # Handle profile picture upload if provided
+        if form.profile_picture.data:
+            picture_file = save_picture(form.profile_picture.data)
+            current_user.profile_picture = picture_file
+        db.session.commit()
+        flash('Değişiklikler kaydedildi.', 'success')
+        return redirect(url_for('edit_profile'))
+    
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    
+    return render_template('edit_profile.html', form=form, flash_success=get_flashed_messages(category_filter=['success']))
+
+
+def save_picture(form_picture):
+    import secrets
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/img', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
